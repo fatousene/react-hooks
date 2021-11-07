5eb329a39c61d5d2e10d0ebfff254b4fbb7e7d84
fatousene <sene0299@gmail.com>
2021-11-07 21:18:45 +0100

    hooks

---
+++ src/assets/mookData.js
@@ -0,0 +1,36 @@
+export const moviesList = [
+    {
+      title: "VIKINGS series Season 1 || Epsoide 1",
+      posterUrl:"http://cima4u.tv/wp-content/uploads/1-71.jpg",
+      description: `Now You can watch the first epsoide of the 
+      action series Vikings  wish you interesting watch`,
+      rate: 5,
+    },
+    {
+      title: `Aquaman (2018)`,
+      posterUrl: `https://pluspng.com/img-png/aquaman-png-aquaman-png-clipart-666.png`,
+      description: `Arthur Curry learns that he is the heir to the underwater 
+                  kingdom of Atlantis,and must step forward to lead his people
+                   and be a hero to the world.`,
+      rate: 3,
+    },
+    {
+      title: `Wonder Woman (2017)`,
+      posterUrl: `https://image.ibb.co/dHdAkS/Wonder_Woman_640x320.jpg`,
+      description: `When a pilot crashes and tells of conflict in the 
+                  outside world, Diana,an Amazonian warrior in training,
+                   leaves home to fight a war, discovering her full powers and true destiny`,
+      rate: 4,
+    },
+    {
+      title: `Spiderman: Homecoming (2018)`,
+      posterUrl: `https://image.ibb.co/da7xX7/spiderman_homecoming_640x320.jpg`,
+      description: `For the first time in his cinematic history, Spider-Man, the
+                   friendly neighborhood hero,is unmasked and can no longer 
+                   separate his normal life from his heavy responsibilities 
+                   as a superhero.When he asks Doctor Strange for help, the 
+                   stakes get even more dangerous,forcing him to find out what
+                   being Spider-Man really means.`,
+      rate: 3,
+    },
+  ];
